9a70b66013a25a7d7d1c3d952eb9500199561231
hongy25 <583699747@qq.com>
2024-01-10 16:42:54 +0800

    add: kafka拉模式消费者

--- src/main/java/com/hy/entity/DBLogMessage.java
+++ src/main/java/com/hy/entity/DBLogMessage.java
@@ -1,6 +1,10 @@
 package com.hy.entity;
 
+import sun.security.krb5.internal.crypto.Des;
+
 import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Map;
 
 /**
  * Description: 清洗后的日志
@@ -9,4 +13,131 @@ import java.io.Serializable;
  */
 public class DBLogMessage implements Serializable {
     private static final long serialVersionUID = 1L;
+
+    private LocalDateTime timestamp;
+    private Event event;
+    private String[] tags;
+    private String message;
+    private Map<String, String> fields;
+    private Destination destination;
+    private Client client;
+    private User user;
+    private String query;
+    private SqlOut sqlOut;
+    private String path;
+    private String type;
+    private Result result;
+    private Organization organization;
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public Event getEvent() {
+        return event;
+    }
+
+    public void setEvent(Event event) {
+        this.event = event;
+    }
+
+    public String[] getTags() {
+        return tags;
+    }
+
+    public void setTags(String[] tags) {
+        this.tags = tags;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Map<String, String> getFields() {
+        return fields;
+    }
+
+    public void setFields(Map<String, String> fields) {
+        this.fields = fields;
+    }
+
+    public Destination getDestination() {
+        return destination;
+    }
+
+    public void setDestination(Destination destination) {
+        this.destination = destination;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getQuery() {
+        return query;
+    }
+
+    public void setQuery(String query) {
+        this.query = query;
+    }
+
+    public SqlOut getSqlOut() {
+        return sqlOut;
+    }
+
+    public void setSqlOut(SqlOut sqlOut) {
+        this.sqlOut = sqlOut;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Result getResult() {
+        return result;
+    }
+
+    public void setResult(Result result) {
+        this.result = result;
+    }
+
+    public Organization getOrganization() {
+        return organization;
+    }
+
+    public void setOrganization(Organization organization) {
+        this.organization = organization;
+    }
 }
---
+++ src/main/java/com/hy/kafka/KafkaPollConsumer.java
@@ -0,0 +1,78 @@
+package com.hy.kafka;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hy.disruptor.OriginLogDisruptor;
+import com.hy.entity.OriginLogMessage;
+import com.lmax.disruptor.RingBuffer;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
+/**
+ * Description: 拉模式的kafka消费者
+ * Author: yhong
+ * Date: 2024/1/10
+ */
+@Slf4j
+@Component
+public class KafkaPollConsumer {
+    private KafkaConsumer<String, String> consumer;
+
+    @Autowired
+    private OriginLogDisruptor disruptor;
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    public KafkaPollConsumer() {
+        this.consumer = null;
+    }
+
+    @PostConstruct
+    public void init() {
+        initializeConsumer();
+        startConsuming();
+    }
+
+
+    public void initializeConsumer() {
+        Properties props = new Properties();
+        // 配置Kafka连接属性
+        props.put("bootstrap.servers", "192.168.174.132:9092");
+        props.put("group.id", "log-info-group");
+        props.put("max.poll.records", "500"); // 设置每次拉取的最大记录数为100
+        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        this.consumer = new KafkaConsumer<>(props);
+        consumer.subscribe(Collections.singletonList("log-info"));
+    }
+
+
+    @Async("asyncExecutor")
+    public void startConsuming() {
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(2000));
+            for (ConsumerRecord<String, String> record : records) {
+                OriginLogMessage message = new OriginLogMessage();
+                message.setOriginLogMessage(record.value());
+                RingBuffer<OriginLogMessage> ringBuffer = disruptor.getDisruptor().getRingBuffer();
+                long sequence = ringBuffer.next();
+                try {
+                    OriginLogMessage event = ringBuffer.get(sequence);
+                    event.setOriginLogMessage(message.getOriginLogMessage());
+                } finally {
+                    ringBuffer.publish(sequence);
+                }
+            }
+        }
+    }
+
+}
